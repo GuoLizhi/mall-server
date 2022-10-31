7c5e0a408885e7c9057562068bc58305cc0dd746
GuoLizhi <lizhi.guo@foxmail.com>
2022-10-31 17:28:20 +0800

    feat: update validate pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto, RegistDto } from './user.dto';
 import { UserService } from './user.service';
 
@@ -7,13 +7,13 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('/login')
-  async login(loginDto: LoginDto) {
+  async login(@Body() loginDto: LoginDto) {
     const { username, password } = loginDto;
     return this.userService.findOne(username, password);
   }
 
   @Post('/regist')
-  async regist(registDto: RegistDto) {
+  async regist(@Body() registDto: RegistDto) {
     return this.userService.save(registDto);
   }
 }
--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,8 +1,14 @@
+import { IsString } from 'class-validator';
+
 export class LoginDto {
+  @IsString()
   readonly username: string;
+
+  @IsString()
   readonly password: string;
 }
 
 export class RegistDto extends LoginDto {
+  @IsString()
   readonly nickname: string;
 }
